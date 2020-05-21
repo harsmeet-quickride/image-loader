04e33879e8ee757cc97be3504462d266d4fadbe6
Rajat Sangrame <rajatsangrame@gmail.com>
2020-05-21 13:33:08 +0530

    added runtime permission

--- app/src/main/java/com/example/imagessubredditviewer/MainActivity.java
+++ app/src/main/java/com/example/imagessubredditviewer/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.imagessubredditviewer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -31,11 +35,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        askPermission();
         initData();
-        makeServerCall();
-
+        if (isPermissionGranted()) {
+            makeServerCall();
+        }
     }
 
     private void initData() {
@@ -47,12 +50,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void askPermission() {
-
-    }
-
     private void makeServerCall() {
-
         RetrofitClient.getInstance(this).getHeadlines().enqueue(new Callback<ApiResponse>() {
             @Override
             public void onResponse(Call<ApiResponse> call, Response<ApiResponse> response) {
@@ -73,4 +71,29 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    public boolean isPermissionGranted() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED) {
+                Log.i(TAG, "Permission is granted");
+                return true;
+            } else {
+                Log.i(TAG, "Permission is revoked");
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                return false;
+            }
+        } else {
+            Log.i(TAG, "Permission is granted");
+            return true;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            Log.i(TAG, "Permission: " + permissions[0] + "was " + grantResults[0]);
+            makeServerCall();
+        }
+    }
 }
